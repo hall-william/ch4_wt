8f95621d92a6c1bb3893364a05a7f6bcf7b115f9
William Hall <whall20@uncc.edu>
2017-10-10 16:00:11 +0000

    Finished Section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,9 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
+  def edit
+    @movie = Movie.find params[:id]
+  end
   
   def update
     @movie = Movie.find params[:id]
@@ -15,6 +18,13 @@ class MoviesController < ApplicationController
     @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.delete
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 
   def index
